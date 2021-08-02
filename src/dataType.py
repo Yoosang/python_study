8a32da440b48df495c10cc25b752838da4556b29
usang <usang6325@gmail.com>
2021-08-02 20:26:06 +0900

    자료형

---
+++ src/dataType.py
@@ -0,0 +1,31 @@
+# 숫자형
+print(5)
+print(10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(4*(2+3))
+
+# 문자형
+print('문자형')
+print("맥북")
+print("반복" * 9)
+
+# boolean
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(not True)
+print(not(5 > 10))
+
+#변수
+animal = "dog"
+name = "john"
+age = 4
+hobby = "tracking"
+is_adult = (age>3)
+
+print("우리집" + animal + "의 이름은" + name +"입니다")
+print(name + "은 " + str(age) + "살이며," + hobby + "를 좋아합니다")
+print(name + "은 어른인가? " + str(is_adult))
